fb75fd5c992f7c91a61015c115b534dac120ca95
inky <git@wo-class.cn>
2024-09-10 22:30:23 +0800

    cpp: move mod.hpp

--- BlackMythA11yCpp/xmake.lua
+++ BlackMythA11yCpp/xmake.lua
@@ -2,7 +2,5 @@ local projectName = "BlackMythA11yCpp"
 
 target(projectName)
     add_rules("ue4ss.mod")
-    add_includedirs(".")
     add_includedirs("include")
-    add_headerfiles("*.hpp")
     add_files("*.cpp") 
