ac555e59027b4c7f4e399886426f74f549011d03
inky <git@wo-class.cn>
2024-09-11 18:29:47 +0800

    lua: 获取焦点文本

--- BlackMythA11y/Scripts/main.lua
+++ BlackMythA11y/Scripts/main.lua
@@ -110,13 +110,16 @@ local function InitManagedHook()
         Button = pContext:get()
         -- TODO: 根据原因筛选相应，消除重复的事件
         InFocusEvent = pInFocusEvent:get()
-        -- WkUtils.PrintUObject(UGSE_UMGFuncLib)
+        -- WkUtils.PrintUObject(TxtName_name)
         -- https://github.com/UE4SS-RE/RE-UE4SS/issues/378
         -- local UGSE_UMGFuncLib = StaticFindObject("/Script/UnrealExtent.Default__GSE_UMGFuncLib")
         -- Cause = UGSE_UMGFuncLib:GetFocusEventCause(InFocusEvent)
+
         print(string.format("OnAddedToFocusPath(%s): %s\n", tostring(InFocusEvent:GetFullName()), tostring(Button:GetFullName())))
+        TxtName = Button.BI_TextLoop.Content
+        print(string.format("\t%s\n", tostring(TxtName:GetContent():ToString())))
     end
-    -- RegisterHook("/Script/b1-Managed.BUI_Button:OnAddedToFocusPath", OnAddedToFocusPath_Hook)
+    RegisterHook("/Script/b1-Managed.BUI_Button:OnAddedToFocusPath", OnAddedToFocusPath_Hook)
     -- RegisterHook("/Script/b1-Managed.BUI_Widget:OnAddedToFocusPath", OnAddedToFocusPath_Hook)
     -- 返回的是上一步的对象
     -- RegisterHook("/Script/b1-Managed.BUI_Button:OnCustomWidgetNavigation", function(Context, Navigation)
