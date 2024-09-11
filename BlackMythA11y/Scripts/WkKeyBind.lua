09beeb8e6f9a9ada32ac54336efac72721dd1bac
inky <git@wo-class.cn>
2024-09-11 21:20:03 +0800

    lua: 拆分 Scripts\WkKeyBind.lua

---
+++ BlackMythA11y/Scripts/WkKeyBind.lua
@@ -0,0 +1,92 @@
+-- SPDX-License-Identifier: MIT
+-- WuKong KeyBind Hook
+local SubModeName = "[BlackMythA11y.WkKeyBind] "
+-- Author: inkydragon
+local UEHelpers = require("UEHelpers")
+local WkUtils = require("WkUtils")
+local WkKeyBind = {}
+-- Global Var
+local GetGameplayStatics = UEHelpers.GetGameplayStatics
+local KeyBindArgMap = {}
+
+
+local function PrintCanvas(widget)
+    if widget and widget:IsValid() then
+        local slot= WidgetLib:SlotAsCanvasSlot(widget)
+        WkUtils.PrintUObject(widget)
+        print(tostring(widget:GetFullName()))
+        print(tostring(slot:GetFullName()))
+	end
+end
+
+local function DumpInfo()
+    print(SubModeName.."DumpInfo...\n")
+    WidgetLib = StaticFindObject("/Script/UMG.Default__WidgetLayoutLibrary")
+
+    WkUtils.PrintGameVersion()
+
+    local UWorld = UEHelpers.GetWorld()
+    -- WkUtils.DumpAllLevels()
+
+    -- 1. 检查当前关卡的名称
+    -- if UWorld then
+    --     print(string.format("UWorld:GetMapName: %s\n", UWorld:GetMapName()))
+    -- else
+    --     print("Null UWorld")
+    -- end
+    
+    -- 2. 检查当前的游戏模式或状态
+    print(string.format(SubModeName.."GetFullName: %s\n", GetGameplayStatics():GetFullName()))
+    print(string.format(SubModeName.."GetPlatformName: %s\n", GetGameplayStatics():GetPlatformName():ToString()))
+    local GameMode = GetGameplayStatics():GetGameMode(UWorld)
+    print(string.format(SubModeName.."GetLevel: %s\n", GameMode:GetLevel():GetFullName()))
+
+    WkUtils.PrintUIPage()
+    -- print(SubModeName.."-----------\n")
+    -- WkUtils.PrintGameInfo()
+    
+    print(SubModeName.."Dump UI...\n")
+    BattleMainUI = FindFirstOf("BUI_BattleMain_C")
+	if not BattleMainUI:IsValid() then
+		print(SubModeName.."Game not ready, Abort\n")
+		return false
+	end
+    prefix = BattleMainUI:GetFullName():gsub("([^ ]*) (.*)","%2")..".WidgetTree." -- BattleMainUI object name
+    
+    BUI_BattleMain_C_WidgetTree = StaticFindObject(prefix.."PlayerStCon")
+
+    hpbar=StaticFindObject(prefix.."PlayerStCon")
+    fpbar=StaticFindObject(prefix.."BI_PlayerStickLevel")
+    fpbar2=StaticFindObject(prefix.."BI_DaShengStickLevel")--大圣套棍势
+    --StaticFindObject(prefix.."BI_DaShengStickLevel"):SetVisibility(0)
+    --StaticFindObject(prefix.."BI_Trans"):SetVisibility(0)
+    skillshortcut=StaticFindObject(prefix.."BI_ShortcutSkill")
+    itemshortcut=StaticFindObject(prefix.."BI_ShortcutItem")
+    treasureshortcut=StaticFindObject(prefix.."BI_Treasure")
+    rzdshortcut=StaticFindObject(prefix.."BI_RZDSkill")
+
+    PrintCanvas(hpbar)
+    PrintCanvas(fpbar)
+end
+-- KeyBindArgMap[Key.F11] = DumpInfo
+
+-- 测试调用 Cpp 注入的模块
+local function CallCppModTest()
+    print(SubModeName.."CallCppModTest...\n")
+
+    print(string.format(SubModeName.."A11yTolk: %s\n", A11yTolk))
+    local Major, Minor, Hotfix = A11yTolk:GetVersion()
+    print(string.format(SubModeName.."A11yTolk v%d.%d.%d\n", Major, Minor, Hotfix))
+    print(string.format(SubModeName.."A11yTolk:Speak(string)\n"))
+    A11yTolk:Speak("aaaaa")
+end
+KeyBindArgMap[Key.F11] = CallCppModTest
+
+
+function WkKeyBind.init()
+    for key, func in pairs(KeyBindArgMap) do --actualcode
+        RegisterKeyBind(key, func)
+    end
+end
+
+return WkKeyBind
--- BlackMythA11y/Scripts/main.lua
+++ BlackMythA11y/Scripts/main.lua
@@ -2,88 +2,11 @@
 -- BlackMythA11y Mod
 local ModName = "[BlackMythA11y] "
 -- Author: inkydragon
-local UEHelpers = require("UEHelpers")
 local WkUtils = require("WkUtils")
 local WkUIHook = require("WkUIHook")
-
-local GetGameplayStatics = UEHelpers.GetGameplayStatics
+local WkKeyBind = require("WkKeyBind")
 -- Global Var
-local WidgetLib = nil
 local bModInit = false
-
-
-local function PrintCanvas(widget)
-    if widget and widget:IsValid() then
-        local slot= WidgetLib:SlotAsCanvasSlot(widget)
-        WkUtils.PrintUObject(widget)
-        print(tostring(widget:GetFullName()))
-        print(tostring(slot:GetFullName()))
-	end
-end
-
-local function DumpInfo()
-    print(ModName.."DumpInfo...\n")
-    WidgetLib = StaticFindObject("/Script/UMG.Default__WidgetLayoutLibrary")
-
-    WkUtils.PrintGameVersion()
-
-    local UWorld = UEHelpers.GetWorld()
-    -- WkUtils.DumpAllLevels()
-
-    -- 1. 检查当前关卡的名称
-    -- if UWorld then
-    --     print(string.format("UWorld:GetMapName: %s\n", UWorld:GetMapName()))
-    -- else
-    --     print("Null UWorld")
-    -- end
-    
-    -- 2. 检查当前的游戏模式或状态
-    print(string.format(ModName.."GetFullName: %s\n", GetGameplayStatics():GetFullName()))
-    print(string.format(ModName.."GetPlatformName: %s\n", GetGameplayStatics():GetPlatformName():ToString()))
-    local GameMode = GetGameplayStatics():GetGameMode(UWorld)
-    print(string.format(ModName.."GetLevel: %s\n", GameMode:GetLevel():GetFullName()))
-
-    WkUtils.PrintUIPage()
-    -- print(ModName.."-----------\n")
-    -- WkUtils.PrintGameInfo()
-    
-    print(ModName.."Dump UI...\n")
-    BattleMainUI = FindFirstOf("BUI_BattleMain_C")
-	if not BattleMainUI:IsValid() then
-		print(ModName.."Game not ready, Abort\n")
-		return false
-	end
-    prefix = BattleMainUI:GetFullName():gsub("([^ ]*) (.*)","%2")..".WidgetTree." -- BattleMainUI object name
-    
-    BUI_BattleMain_C_WidgetTree = StaticFindObject(prefix.."PlayerStCon")
-
-    hpbar=StaticFindObject(prefix.."PlayerStCon")
-    fpbar=StaticFindObject(prefix.."BI_PlayerStickLevel")
-    fpbar2=StaticFindObject(prefix.."BI_DaShengStickLevel")--大圣套棍势
-    --StaticFindObject(prefix.."BI_DaShengStickLevel"):SetVisibility(0)
-    --StaticFindObject(prefix.."BI_Trans"):SetVisibility(0)
-    skillshortcut=StaticFindObject(prefix.."BI_ShortcutSkill")
-    itemshortcut=StaticFindObject(prefix.."BI_ShortcutItem")
-    treasureshortcut=StaticFindObject(prefix.."BI_Treasure")
-    rzdshortcut=StaticFindObject(prefix.."BI_RZDSkill")
-
-    PrintCanvas(hpbar)
-    PrintCanvas(fpbar)
-end
-
--- RegisterKeyBind(Key.F11, DumpInfo)
-
--- 测试调用 Cpp 注入的模块
-local function CallCppModTest()
-    print(ModName.."CallCppModTest...\n")
-
-    print(string.format(ModName.."A11yTolk: %s\n", A11yTolk))
-    local Major, Minor, Hotfix = A11yTolk:GetVersion()
-    print(string.format(ModName.."A11yTolk v%d.%d.%d\n", Major, Minor, Hotfix))
-    print(string.format(ModName.."A11yTolk:Speak(string)\n"))
-    A11yTolk:Speak("aaaaa")
-end
-RegisterKeyBind(Key.F11, CallCppModTest)
 
 
 local function InitManagedHook()
@@ -118,3 +41,5 @@ end
 --     print(string.format("SetText: %s\n", tostring(Button:GetFullName())))
 --     print(string.format("\t%s\n", tostring(InText:get():ToString())))
 -- end)
+
+WkKeyBind.init()
