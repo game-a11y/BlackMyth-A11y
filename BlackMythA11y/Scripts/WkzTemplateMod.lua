3a519acf96a2a86945c1586abf785e1005d62758
inky <git@wo-class.cn>
2024-09-11 21:29:05 +0800

    lua: Create WkzTemplateMod.lua

---
+++ BlackMythA11y/Scripts/WkzTemplateMod.lua
@@ -0,0 +1,13 @@
+-- SPDX-License-Identifier: MIT
+-- WuKong TemplateMod
+-- Author: inkydragon
+local SubModeName = "[BlackMythA11y.WkzTemplateMod] "
+-- [[require Global]]
+
+-- [[require Local]]
+
+-- [[Global Var]]
+local WkzTemplateMod = {}
+
+
+return WkzTemplateMod
