a9cada85e4ac2764482f5e3414fcffd7ac5e9964
inky <git@wo-class.cn>
2024-09-17 15:13:43 +0800

    lua: Create WkConfig.lua

---
+++ BlackMythA11y/Scripts/WkConfig.lua
@@ -0,0 +1,17 @@
+-- SPDX-License-Identifier: MIT
+-- WuKong Config Mod: 配置文件
+-- Author: inkydragon @ Bilibili
+local SubModeName = "[BlackMythA11y.WkConfig] "
+-- [[require Global]]
+
+-- [[require Local]]
+
+-- [[Global Var]]
+local WkConfig = {}
+
+
+-- 是否为性能测试工具 模式
+WkConfig.IsBencmarkTools = true
+
+
+return WkConfig
