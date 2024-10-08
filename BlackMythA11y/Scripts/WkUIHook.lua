13b58cf3e439ba0047d5e27ad77f3c8a3610ea54
inky <git@wo-class.cn>
2024-10-08 22:19:06 +0800

    lua: 拆分 UI hook，游戏中:交互/弹出提示

--- BlackMythA11y/Scripts/WkUIHook.lua
+++ BlackMythA11y/Scripts/WkUIHook.lua
@@ -107,34 +107,8 @@ require("UIHooks.InventoryItem")
 require("UIHooks.TravelNotes")
 -- 游戏中:背包3:设置    SKIP
 
-
--- TODO: 游戏中:(土地庙)提示
---[[
-    E Offer Incense:    TextBlock /BUI_BattleInfo_C_2147446787.WidgetTree.BI_InteractIcon.WidgetTree.InteractIcon_98_0.WidgetTree.TxtTips
-
-    CanvasPanel /.BUI_BattleInfo_C_2147446787.WidgetTree.BI_InteractIcon.WidgetTree.InteractIcon_98_0.WidgetTree.TipsCon
-        可以用 TipsCon 的 StructProperty /Script/UMG.Widget:RenderTransform
-            StructProperty /Script/UMG.WidgetTransform:Translation - X
-            < 0 == -50 隐藏
-            ==0     显示
-    或 CanvasPanel /Engine/Transient.GameEngine_2147482611:BGW_GameInstance_B1_2147482576.BUI_BattleInfo_C_2147446787.WidgetTree.BI_InteractIcon.WidgetTree.InteractIcon_98_0.WidgetTree.TipsRoot
-        RenderOpacity 
-]]
--- TODO: 游戏中:可拾取物品 提示
---[[
-    Gather:    TextBlock /BUI_BattleInfo_C_2147446787.WidgetTree.BI_InteractIcon.WidgetTree.InteractIcon_98_1.WidgetTree.TxtTips
-]]
-
--- TODO: 游戏中:物品掉落\经验
---[[
-物品掉落
-    BUI_DropMain_C_2147444306.WidgetTree.BI_DropManual.WidgetTree.TxtName
-    BUI_DropMain_C_2147444306.WidgetTree.BI_DropManual.WidgetTree.TxtSubDecs
-    
-经验
-    BUI_DropMain_C_2147444306.WidgetTree.BI_DropExpProg_V2.WidgetTree.TxtLevel
-    BUI_DropMain_C_2147444306.WidgetTree.BI_DropExpProg_V2.WidgetTree.TxtNumExp
-]]
+-- 游戏中:交互/弹出提示
+require("UIHooks.InteractIcon")
 
 
 -- [[ Hook 函数 ]] --------------------------------------------------------------
