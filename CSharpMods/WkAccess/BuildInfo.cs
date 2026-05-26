b794f425cb10452cc8dfcc912d2ebe91d0b1bd53
Chengyu HAN <cyhan.dev@outlook.com>
2026-05-26 22:36:49 +0800

    cs: 更新日期

--- CSharpMods/WkAccess/BuildInfo.cs
+++ CSharpMods/WkAccess/BuildInfo.cs
@@ -4,7 +4,7 @@ internal static class BuildInfo
 {
     internal const string ModName = "WkAccess";
     internal const string ModDevName = "Game-A11y";
-    internal const string ModVersion = "2025.08.21";
-    internal const string ModCopyright = "Copyright (c) 2025 inkydragon @ Github";
+    internal const string ModVersion = "2026.05.26";
+    internal const string ModCopyright = "Copyright (c) 2025-2026 inkydragon @ Github";
     internal const string ModDescription = "A CSharp accessbility mod for BlackMyth: Wukong.";
 }
