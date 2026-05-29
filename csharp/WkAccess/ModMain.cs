b65e48dc5025f52d2bad86fe7b4e13b8bb2be073
Chengyu HAN <cyhan.dev@outlook.com>
2026-05-29 23:22:15 +0800

    ref: ModMain 垂直间距整理，Stop 顺序与 Start 对齐

--- csharp/WkAccess/ModMain.cs
+++ csharp/WkAccess/ModMain.cs
@@ -1,4 +1,3 @@
-using CSharpModBase.Input;
 using HarmonyLib;
 
 namespace WkAccess;
@@ -8,33 +7,47 @@ public sealed class WkAccess : ICSharpMod
     static readonly Harmony _harmony = new($"{BuildInfo.ModName}.{BuildInfo.ModVersion}");
 
     public string Name => ModName;
+    public string ModNameFull => $"{ModName} MOD";
     public string Version => ModVersion;
 
     public WkAccess()
     {
         A11yLog.Init(fileLogDir: Path.Combine(
             AppDomain.CurrentDomain.BaseDirectory ?? ".", Common.ModDir, BuildInfo.ModName));
-        A11yLog.Info($"{Name} Constructor called @ {DateTime.Now}");
+
+        // 输出游戏版本信息
+        DebugCommands.PrintGameBuildInfo();
+        A11yLog.Info($"{ModNameFull} Constructor called @ {DateTime.Now}");
     }
 
+    /// <summary>
+    /// 初始化 MOD 模块
+    /// </summary>
+    /// <remarks>注意：热加载模组，会再次调用此函数！</remarks>
     public void Init()
     {
-        DebugCommands.PrintGameBuildInfo();
+        A11yLog.Info($"{ModNameFull} Init() Start:");
         DebugCommands.PrintModBuildInfo();
+
         KeyBindings.RegisterAll();
         B1WidgetExtractors.RegisterAll(UIScreenTextProvider.Register);
+
         B1.SceneMonitor.Start();
         InteractMonitor.Start();
         _harmony.PatchAll();
-        A11yLog.Info($"{Name} Init()");
+
+        A11yLog.Info($"{ModNameFull} Init END.");
     }
 
     public void DeInit()
     {
-        A11yLog.Info($"{Name} DeInit");
+        A11yLog.Info($"{ModNameFull} DeInit() Start:");
+
         _harmony.UnpatchAll();
-        B1.SceneMonitor.Stop();
         InteractMonitor.Stop();
+        B1.SceneMonitor.Stop();
+        
+
         A11yLog.Deinit();
     }
 }
