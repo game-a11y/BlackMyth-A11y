d7c854c93a0a4fbe3d84cde8b9d820253eb6773b
Chengyu HAN <cyhan.dev@outlook.com>
2026-06-04 17:33:45 +0800

    cs(A11yMod.Patches): 提取 H_MouseDown + H_KeyUp 到 Patches/BUI_Button.cs，移除 UIInputLogger.cs

--- csharp/WkAccess/A11yMod/Patches/BUI_Button.cs
+++ csharp/WkAccess/A11yMod/Patches/BUI_Button.cs
@@ -48,3 +48,31 @@ static class H_FocusLeave
         }
     }
 }
+
+[HarmonyPatch(typeof(BUI_Button), "OnMouseButtonDown_Implementation")]
+static class H_MouseDown
+{
+    static void Postfix(BUI_Button __instance)
+    {
+        var gsid = __instance.GetGSID();
+        if (gsid >= 0)
+        {
+            var cn = WkUtils.GetClassName(__instance);
+            A11yLog.Debug($"[UI.Click] 点击 {cn}#{gsid}");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(BUI_Button), "OnKeyUp_Implementation")]
+static class H_KeyUp
+{
+    static void Postfix(BUI_Button __instance)
+    {
+        var gsid = __instance.GetGSID();
+        if (gsid >= 0)
+        {
+            var cn = WkUtils.GetClassName(__instance);
+            A11yLog.Debug($"[UI.KeyUp] 按键 {cn}#{gsid}");
+        }
+    }
+}
